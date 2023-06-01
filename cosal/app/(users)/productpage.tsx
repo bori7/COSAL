647c956f332db57cfde410bbbff446d25206c659
Bos8086 <Boma.obomanu86@gmail.com>
2023-06-01 09:40:32 +0100

    Product Page

--- cosal/app/(users)/productpage.tsx
+++ cosal/app/(users)/productpage.tsx
@@ -3,15 +3,27 @@ import React, { useEffect } from "react";
 import { View, Text, ImageBackground, StyleSheet } from "react-native";
 import { PRODUCTS, ProductType } from "../../constants/Objects";
 import { Colors } from "react-native/Libraries/NewAppScreen";
-import { AntDesign, Feather, SimpleLineIcons } from "@expo/vector-icons";
+import {
+  AntDesign,
+  Feather,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import CartIcon from "../../components/users/CartIcon";
 
 export default function ProductPage() {
-  const { productId } = useLocalSearchParams();
+
+
+
+  const { productId, title, rating, category, price, item } =
+    useLocalSearchParams();
+
   const router = useRouter();
   useEffect(() => {
     if (productId) {
       console.log(productId);
+      console.log(title);
     }
   }, [productId]);
   return (
@@ -19,7 +31,7 @@ export default function ProductPage() {
       <View style={{ height: 350 }}>
         {productId && (
           <ImageBackground
-            source={PRODUCTS?.[productId].uri}
+            source={PRODUCTS[productId]?.uri}
             style={styles.background}
           >
             <View style={styles.mainView}>
@@ -50,7 +62,100 @@ export default function ProductPage() {
           </ImageBackground>
         )}
       </View>
-      <Text>ProductPAGE</Text>
+      <View>
+        <Text style={styles.MainText}>{title}</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <AntDesign name="star" color={"#F8A23B"} size={15} />
+            <Text style={{ marginHorizontal: 6, color: "grey" }}>{rating}</Text>
+            <Text style={{ color: "grey" }}>Ratings</Text>
+          </View>
+
+          <View>
+            <Text style={{ marginHorizontal: 6, color: "grey" }}>
+              2.3K + Reviews
+            </Text>
+          </View>
+
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Text style={{ color: "grey" }}>{item}+</Text>
+            <Text style={{ marginHorizontal: 6, color: "grey" }}>Sold</Text>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: "row",justifyContent: "space-evenly", }}>
+          <View
+            style={{
+              borderColor: "black",
+              borderWidth: 1,
+              height: 50,
+              width: 40,
+              justifyContent: "center",   
+            }}
+          >
+            <MaterialIcons
+              name="house-siding"
+              size={20}
+              style={{ alignSelf: "center" }}
+            />
+          </View>
+          <View
+            style={{
+              borderColor: "black",
+              borderWidth: 1,
+              height: 50,
+              width: 150,
+              justifyContent: "space-evenly",
+              flexDirection: "row",
+              backgroundColor:"black"
+            }}
+          >
+             <MaterialCommunityIcons
+              name="paperclip"
+              size={20}
+              color={"white"}
+              style={{
+                transform: [{ rotate: "90deg" }],
+               
+                height: 30,
+                width: 30,
+                alignSelf: "center",
+                marginTop:10
+              }}
+            />
+            <Text style={{ alignSelf: "center",color:"white" }}>Cosell</Text>
+           
+            <SimpleLineIcons name="arrow-down" size={15} color={"white"} style={{alignSelf:"center"}}/>
+          </View>
+          <View  style={{
+              borderColor: "black",
+              borderWidth: 1,
+              height: 50,
+              width: 110,
+              justifyContent: "center",
+              flexDirection:"row"
+              
+            }}>
+              <SimpleLineIcons name="basket" size={20} style={{alignSelf: "center"}}/>
+            <Text style={{alignSelf: "center"}}>
+                Add to Cart
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -63,5 +168,10 @@ const styles = StyleSheet.create({
   mainView: {
     marginTop: 50,
     backgroundColor: Colors.light.transparent,
+  },
+  MainText: {
+    fontSize: 40,
+    color: "black",
+    fontWeight: "bold",
   },
 });
